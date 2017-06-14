378bf37ab6f07c46753c3022e6129d2ef592a9d2
Anisha <anishapal006@gmail.com>
2017-06-14 17:21:34 +0530

    ListView

--- app/src/main/java/com/example/shourya/myapp/firstFragment.java
+++ app/src/main/java/com/example/shourya/myapp/firstFragment.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by shourya on 28/5/17.
@@ -19,6 +21,16 @@ public class firstFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView= inflater.inflate(R.layout.first_layout, container, false);
+        String [] menu={"Up",
+        "Despicable Me",
+        "Finding Nemo"};
+        ListView listview=(ListView)myView.findViewById(R.id.list);
+        ArrayAdapter<String>listViewAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                menu
+        );
+        listview.setAdapter(listViewAdapter);
         return myView;
     }
 
