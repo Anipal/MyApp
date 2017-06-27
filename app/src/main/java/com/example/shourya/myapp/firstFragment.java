5762ead79b1ff8ae76790676770babc9846818a2
Anisha <anishapal006@gmail.com>
2017-06-27 21:41:46 +0530

    Image in listView

--- app/src/main/java/com/example/shourya/myapp/firstFragment.java
+++ app/src/main/java/com/example/shourya/myapp/firstFragment.java
@@ -8,12 +8,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.app.Activity;
 
 /**
  * Created by shourya on 28/5/17.
  */
 
 public class firstFragment extends Fragment{
+    ListView list;
 
     View myView;
 
@@ -21,16 +29,29 @@ public class firstFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView= inflater.inflate(R.layout.first_layout, container, false);
-        String [] menu={"Up",
+        String [] web={"Up",
                 "Despicable Me",
-                "Finding Nemo"};
-        ListView listview=(ListView)myView.findViewById(R.id.list);
-        ArrayAdapter<String>listViewAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                menu
-        );
-        listview.setAdapter(listViewAdapter);
+                "Finding Nemo","Kung Fu Panda","Frozen"};
+        Integer[] imageId = {
+                R.drawable.image1,
+                R.drawable.image2,
+                R.drawable.image3,
+                R.drawable.image4,
+                R.drawable.image5,
+        };
+        CustomList adapter = new
+                CustomList(getActivity(), web, imageId);
+        list=(ListView)myView.findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+               
+
+            }
+        });
         return myView;
     }
 
